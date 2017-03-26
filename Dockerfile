d37e80729f8b4e7d9ca0f75254c79ca661e608b8
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-03-26 13:15:24 +0200

    Add .env to ignore

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
-FROM gocd/docker-gocd-agent-debian-8:v17.3.0
+FROM debian:jessie
+MAINTAINER <mail@sebastian-hutter.ch>
 
+# build arguments
+ARG GOCD_AGENT_VERSION=17.3.0
+
+# environment variables used for building and entrypoint
+ENV GOCD_DATA=/var/lib/go-agent
+ENV GOCD_HOME=/var/go
+ENV GOCD_LOG=/var/log/go-agent
+ENV GOCD_SCRIPT=/usr/share/go-agent
+ENV DEFAULTS=/etc/default/go-agent
+
+# additional scripts etc
 ARG GAUCHO_VERSION=0.0.1
-
-ENV GO_HOME=/home/go
 
 # install build requirements
 RUN apt-get update \
@@ -12,7 +22,23 @@ RUN apt-get update \
     stable" \
   && curl -fsSL https://download.docker.com/linux/debian/gpg  | apt-key add - \
   && apt-get update \
-  && apt-get install -y gettext jq docker-ce build-essential httpie python-pip \
+  && apt-get install -y gettext jq docker-ce build-essential git httpie python-pip \
+  && rm -rf /var/lib/apt/lists/*
+
+# install requirements for the gocd agents
+RUN echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list \
+ && apt-get update \
+ && apt-get install -y curl jq gettext apt-transport-https git \
+ && apt-get install -y -t jessie-backports ca-certificates-java openjdk-8-jre-headless \
+ && rm -rf /var/lib/apt/lists/*
+
+# install the gocd agent
+# the apt-cache command tries to get the correct debian package version form the
+# specfiied gocd_server_version variable
+RUN echo "deb https://download.gocd.io /" > /etc/apt/sources.list.d/gocd.list \
+  && curl https://download.gocd.io/GOCD-GPG-KEY.asc | apt-key add - \
+  && apt-get update \
+  && apt-get install -y go-agent=$(apt-cache show go-server | grep "Version: ${GOCD_AGENT_VERSION}.*" | head -n 1 | awk '{print $2}') \
   && rm -rf /var/lib/apt/lists/*
 
 # install gaucho script
@@ -31,9 +57,9 @@ COPY build/scripts/* /usr/local/bin/
 RUN chmod +x /usr/local/bin/*.sh
 
 # add entrypoint and ssh config
-ADD build/docker-entrypoint.sh /docker-entrypoint-custom.sh
+ADD build/docker-entrypoint.sh /docker-entrypoint.sh
 ADD build/ssh.config ${GO_HOME}/.ssh/config
-RUN chmod +x /docker-entrypoint-custom.sh \
+RUN chmod +x /docker-entrypoint.sh \
   && chown -R go:go ${GO_HOME}/.ssh
 
-ENTRYPOINT ["/docker-entrypoint-custom.sh"]
+ENTRYPOINT ["/docker-entrypoint.sh"]
--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -9,9 +9,9 @@ ACCESS_TOKEN=$(curl -X POST \
      ${VAULT_SERVER}/v1/auth/approle/login | jq -r .auth.client_token)
 
 # write the private key file for git
-curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_GITHUB_KEY} | jq -r .data.value > ${GO_HOME}/.ssh/id_rsa
-chown go:go ${GO_HOME}/.ssh/id_rsa
-chmod 0600 ${GO_HOME}/.ssh/id_rsa
+curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_GITHUB_KEY} | jq -r .data.value > ${GOCD_HOME}/.ssh/id_rsa
+chown go:go ${GOCD_HOME}/.ssh/id_rsa
+chmod 0600 ${GOCD_HOME}/.ssh/id_rsa
 
 # authenticate against docker hub
 docker_user=$(curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_DOCKER_USER} | jq -r .data.value)
@@ -24,13 +24,28 @@ unset docker_pass
 vault_data=$(curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_RANCHER_API})
 access=$(echo $vault_data | jq -r .data.key)
 secret=$(echo $vault_data | jq -r .data.secret)
-sed -i "s/export RANCHER_ACCESS_KEY=.*/export RANCHER_ACCESS_KEY=${access}/" ${GO_HOME}/.rancher
-sed -i "s/export RANCHER_SECRET_KEY=.*/export RANCHER_SECRET_KEY=${secret}/" ${GO_HOME}/.rancher
-chown go:go ${GO_HOME}/.rancher
-chmod 0600 ${GO_HOME}/.rancher
+sed -i "s/export RANCHER_ACCESS_KEY=.*/export RANCHER_ACCESS_KEY=${access}/" ${GOCD_HOME}/.rancher
+sed -i "s/export RANCHER_SECRET_KEY=.*/export RANCHER_SECRET_KEY=${secret}/" ${GOCD_HOME}/.rancher
+chown go:go ${GOCD_HOME}/.rancher
+chmod 0600 ${GOCD_HOME}/.rancher
 unset vault_data
 unset access
 unset secret
 
-# after configuring the agent execute the original entrypoint
-exec /docker-entrypoint.sh
+# configure the go agent
+# set the go server url
+sed -i "s/GO_SERVER_URL=.*/GO_SERVER_URL=${GO_SERVER_URL}/" "${DEFAULTS}"
+# set the auto registration key
+if [ -n "${GOCD_AGENTAUTOREGISTERKEY}" ]; then
+  mkdir -p "${GOCD_DATA}/config"
+  echo "agent.auto.register.key=${GOCD_AGENTAUTOREGISTERKEY} > ${GOCD_DATA}/config/autoregister.properties"
+  chown -R go:go "${GOCD_DATA}/config"
+fi
+
+# now start the agent
+# we start the agent daemonized (see the /etc/defaults/go-agent DAEMON flag)
+# to keep the container running we tail the go-cd server log files
+su - go -c "${GOCD_SCRIPT}/agent.sh"
+# we sleep 3 seconds to surpress the error message about missing logfiles
+sleep 3
+su - go -c "tail -qF ${GOCD_LOG}/go-agent-bootstrapper.out.log  ${GOCD_LOG}/go-agent-launcher.log"
